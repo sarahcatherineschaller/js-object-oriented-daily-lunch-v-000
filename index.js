1f4f1a54d3751922bb216843abd1628f405c5602
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-20 23:34:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
-let neighborhoodId = 0
-let mealId = 0
-let customerId = 0
-let delveryId = 0
+let neighborhoodId = 0;
+let customerId = 0;
+let mealId = 0;
+let deliveryId = 0;
+
 
 class Neighborhood {
   constructor(name) {
