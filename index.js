faa3187bf0b92935a626586ba556deb6d21a8fd8
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-20 23:32:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
-let neighborhoodId = 0
-let mealId = 0
-let customerId = 0
-let delveryId = 0
+let neighborhoodId = 0;
+let customerId = 0;
+let mealId = 0;
+let deliveryId = 0;
+
 
 class Neighborhood {
-  constructor(name) {
-    this.id = ++neighborhoodId
-    this.name = name
+  constructor (name){
+    this.name = name;
+    this.id = neighborhoodId++;
+
     store.neighborhoods.push(this);
   }
-  deliveries() {
-    return store.deliveries.filter(delivery => {
-      return delivery.neighborhoodId === this.id;
-    });
-  }
-  customers() {
-    return store.customers.filter(customer => {
+  customers(){
+    return store.customers.filter(customer =>{
       return customer.neighborhoodId === this.id;
-    });
+    })
+  }
+  deliveries(){
+    return store.deliveries.filter(delivery =>{
+      return delivery.neighborhoodId === this.id;
+    })
   }
   meals(){
     let mealsArray = this.deliveries().filter(delivery =>{
