7a1d8d199a3a92c185e9b9c421f9acf4ac7c2b30
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-20 23:34:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
 let neighborhoodId = 0;
-let customerId = 0;
 let mealId = 0;
-let deliveryId = 0;
-
+let customerId = 0;
+let delveryId = 0;
 
 class Neighborhood {
   constructor(name) {
